c2baecf7ece49efe1f410a045e63c51d838b77f1
Guppy16 <guptaa@wilsonsschool.sutton.sch.uk>
2020-04-07 02:27:04 +0100

    Added dummy layouts for playlist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+let defaultTextColor = '#fff'
+let defaultStyle = {
+  color: defaultTextColor,
+}
+
+class Aggregate extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
+        <h2 style={{color: '#fff'}}>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div style={{color: defaultTextColor}}>
+        <img></img>
+        <input type="text"/>
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render () {
+    return (
+      <div style={{...defaultStyle, width: "25%", display: 'inline-block', textAlign: 'left'}}>
+        <img />
+        <h3 style={{textAlign: 'center'}}>Playlist Name</h3>
+        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <h1>Shared Spotify Playlist</h1>
+      <Aggregate />
+      <Aggregate />
+      <Filter />
+      <Playlist />
+      <Playlist />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
